a7eee99751d6d6b2ac2a6e30ef89a1b365df4d7f
Alper Keske <alperkeske@Mac.fritz.box>
2025-09-06 00:56:36 +0200

    son1

--- create_servers.sh
+++ create_servers.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
-# filepath: set_vm_ips.sh
+# filepath: create_servers.sh
 
-read -p "Başlangıç VMID: " START
-read -p "Bitiş VMID: " END
+TEMPLATE_ID=9000
+VM_ID_START=100
 
-while read -r VMID IP; do
-    if (( VMID >= START && VMID <= END )); then
-        echo "Setting IP $IP for VM $VMID"
-        qm set $VMID --ipconfig0 ip=$IP/24,gw=192.168.178.1
-    fi
+while read -r NAME IP CORE DISKSIZE BRIDGE MEMORY; do
+    VM_ID=$((VM_ID_START++))
+    echo "Creating $NAME with VMID $VM_ID, IP $IP, $CORE cores, $DISKSIZE disk, bridge $BRIDGE, $MEMORY MB RAM"
+
+    qm clone $TEMPLATE_ID $VM_ID --name $NAME
+
+    qm set $VM_ID \
+        --cores $CORE \
+        --memory $MEMORY \
+        --net0 virtio,bridge=$BRIDGE \
+        --ide0 local-lvm:$DISKSIZE \
+        --ipconfig0 ip=$IP/24,gw=192.168.178.1
+
+    qm start $VM_ID
 done < servers.txt
