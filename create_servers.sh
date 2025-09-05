23d63942cc3b00a9205f207df7f8a2919a5aae51
Alper Keske <alperkeske@Mac.fritz.box>
2025-09-06 00:49:06 +0200

    degisken ip

--- create_servers.sh
+++ create_servers.sh
@@ -1,20 +1,11 @@
 #!/bin/bash
-# filepath: create_servers.sh
+# filepath: set_vm_ips.sh
 
-TEMPLATE_ID=9000
-VM_ID_START=100
+read -p "Başlangıç VMID: " START
+read -p "Bitiş VMID: " END
 
-while read -r NAME IP CORE DISKSIZE BRIDGE MEMORY; do
-    VM_ID=$((VM_ID_START++))
-    echo "Creating $NAME with VMID $VM_ID, IP $IP, $CORE cores, $DISKSIZE disk, bridge $BRIDGE, $MEMORY MB RAM"
-
-    qm clone $TEMPLATE_ID $VM_ID --name $NAME
-
-    qm set $VM_ID \
-        --cores $CORE \
-        --memory $MEMORY \
-        --net0 virtio,bridge=$BRIDGE,ip=$IP/24,gw=192.168.178.1 \
-        --ide0 local-lvm:$DISKSIZE
-
-    qm start $VM_ID
-done < servers.txt
+for (( VMID=START; VMID<=END; VMID++ )); do
+    read -p "VM $VMID için IP adresi: " IP
+    echo "Setting IP $IP for VM $VMID"
+    qm set $VMID --ipconfig0 ip=$IP/24,gw=192.168.178.1
+done
