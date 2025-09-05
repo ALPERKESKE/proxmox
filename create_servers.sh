b61f88f9c4b53846cf8e66d5ba35859243a48151
Alper Keske <alperkeske@Mac.fritz.box>
2025-09-06 00:32:11 +0200

    v2

---
+++ create_servers.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+# filepath: create_servers.sh
+
+TEMPLATE_ID=9000
+VM_ID_START=100
+
+while read -r NAME IP CORE DISKSIZE BRIDGE MEMORY; do
+    VM_ID=$((VM_ID_START++))
+    echo "Creating $NAME with VMID $VM_ID, IP $IP, $CORE cores, $DISKSIZE disk, bridge $BRIDGE, $MEMORY MB RAM"
+
+    qm clone $TEMPLATE_ID $VM_ID --name $NAME
+
+    qm set $VM_ID \
+        --cores $CORE \
+        --memory $MEMORY \
+        --net0 virtio,bridge=$BRIDGE,ip=$IP/24,gw=192.168.178.1 \
+        --ide0 local-lvm:$DISKSIZE
+
+    qm start $VM_ID
+done < servers.txt
